b3ec693ad457fb276576304f36128fb34ad5290c
Israel Bravo <bravoil@mail.uc.edu>
2019-12-11 19:45:12 -0500

    cpu julia

--- chapter05/ImageBlur.cu
+++ chapter05/ImageBlur.cu
@@ -20,22 +20,50 @@
 #define DIM 1024
 #define PI 3.1415926535897932f
 
+__global__ void kernel( unsigned char *ptr ) {
+    // map from threadIdx/BlockIdx to pixel position
+    int x = threadIdx.x + blockIdx.x * blockDim.x;
+    int y = threadIdx.y + blockIdx.y * blockDim.y;
+    int offset = x + y * blockDim.x * gridDim.x;
+
+    __shared__ float    shared[16][16];
+
+    // now calculate the value at that position
+    const float period = 128.0f;
+
+    shared[threadIdx.x][threadIdx.y] =
+            255 * (sinf(x*2.0f*PI/ period) + 1.0f) *
+                  (sinf(y*2.0f*PI/ period) + 1.0f) / 4.0f;
+
+    // removing this syncthreads shows graphically what happens
+    // when it doesn't exist.  this is an example of why we need it.
+    __syncthreads();
+
+    ptr[offset*4 + 0] = 0;
+    ptr[offset*4 + 1] = shared[15-threadIdx.x][15-threadIdx.y];
+    ptr[offset*4 + 2] = 0;
+    ptr[offset*4 + 3] = 255;
+}
+
+// globals needed by the update routine
+struct DataBlock {
+    unsigned char   *dev_bitmap;
+};
+
 struct cuComplex {
     float   r;
     float   i;
     cuComplex( float a, float b ) : r(a), i(b)  {}
-    __device__ float magnitude2( void ) {
-        return r * r + i * i;
-    }
-    __device__ cuComplex operator*(const cuComplex& a) {
+    float magnitude2( void ) { return r * r + i * i; }
+    cuComplex operator*(const cuComplex& a) {
         return cuComplex(r*a.r - i*a.i, i*a.r + r*a.i);
     }
-    __device__ cuComplex operator+(const cuComplex& a) {
+    cuComplex operator+(const cuComplex& a) {
         return cuComplex(r+a.r, i+a.i);
     }
 };
 
-__device__ int julia( int x, int y ) {
+int julia( int x, int y ) {
     const float scale = 1.5;
     float jx = scale * (float)(DIM/2 - x)/(DIM/2);
     float jy = scale * (float)(DIM/2 - y)/(DIM/2);
@@ -53,43 +81,39 @@ __device__ int julia( int x, int y ) {
     return 1;
 }
 
-__global__ void julia_kernel( unsigned char *ptr ) {
-    // map from blockIdx to pixel position
-    int x = blockIdx.x;
-    int y = blockIdx.y;
-    int offset = x + y * gridDim.x;
+void draw( unsigned char *ptr ){
+    for (int y=0; y<DIM; y++) {
+        for (int x=0; x<DIM; x++) {
+            int offset = x + y * DIM;
 
-    // now calculate the value at that position
-    int juliaValue = julia( x, y );
-    ptr[offset*4 + 0] = 255 * juliaValue;
-    ptr[offset*4 + 1] = 0;
-    ptr[offset*4 + 2] = 0;
-    ptr[offset*4 + 3] = 255;
-}
-
-// globals needed by the update routine
-struct DataBlock {
-    unsigned char   *dev_bitmap;
-};
+            int juliaValue = julia( x, y );
+            ptr[offset*4 + 0] = 255 * juliaValue;
+            ptr[offset*4 + 1] = 0;
+            ptr[offset*4 + 2] = 0;
+            ptr[offset*4 + 3] = 255;
+        }
+    }
+ }
 
 int main( void ) {
-    DataBlock   data;
-    CPUBitmap bitmap( DIM, DIM, &data );
-    unsigned char    *dev_bitmap;
+	DataBlock   data;
+	CPUBitmap bitmap( DIM, DIM, &data );
+	unsigned char    *dev_bitmap;
+	draw (dev_bitmap)
 
-    HANDLE_ERROR( cudaMalloc( (void**)&dev_bitmap,
-                              bitmap.image_size() ) );
-    data.dev_bitmap = dev_bitmap;
+	HANDLE_ERROR( cudaMalloc( (void**)&dev_bitmap,
+														bitmap.image_size() ) );
+	data.dev_bitmap = dev_bitmap;
 
-    dim3    grids(DIM/16,DIM/16);
-    dim3    threads(16,16);
-    julia_kernel<<<grids,threads>>>( dev_bitmap );
+	dim3    grids(DIM/16,DIM/16);
+	dim3    threads(16,16);
+	kernel<<<grids,threads>>>( dev_bitmap );
 
-    HANDLE_ERROR( cudaMemcpy( bitmap.get_ptr(), dev_bitmap,
-                              bitmap.image_size(),
-                              cudaMemcpyDeviceToHost ) );
+	HANDLE_ERROR( cudaMemcpy( bitmap.get_ptr(), dev_bitmap,
+														bitmap.image_size(),
+														cudaMemcpyDeviceToHost ) );
 
-    HANDLE_ERROR( cudaFree( dev_bitmap ) );
+	HANDLE_ERROR( cudaFree( dev_bitmap ) );
 
-    bitmap.display_and_exit();
+	bitmap.display_and_exit();
 }
